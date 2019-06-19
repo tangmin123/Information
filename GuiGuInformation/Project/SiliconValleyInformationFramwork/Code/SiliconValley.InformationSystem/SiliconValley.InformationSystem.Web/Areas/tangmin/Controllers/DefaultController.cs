5eba723acf9585aaf329cc6d643c048eebacc50a
tangmin <tangmin@qq.com>
2019-06-19 09:01:25 +0800

    tt

---
+++ GuiGuInformation/Project/SiliconValleyInformationFramwork/Code/SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/tangmin/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiliconValley.InformationSystem.Web.Areas.tangmin.Controllers
+{
+    public class DefaultController : BaseMvcController
+    {
+        // GET: tangmin/Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 第一阶段调研/Project/SiliconValleyInformationFramwork/Code/SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/tangmin/tangminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SiliconValley.InformationSystem.Web.Areas.tangmin
+{
+    public class tangminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "tangmin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "tangmin_default",
+                "tangmin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
